f8602cd83547437edfd7630d6d770aa4c1f46a99
kenosome <kennyj.osorio@gmail.com>
2016-04-09 12:19:53 -0500

    CRUD implementado con API REST

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,45 @@
 class UserController < ApplicationController
 
+  def show
+    @user=User.find(params[:id])
+  end
+
+  def index
+    @users=User.all
+  end
+
+  def destroy
+    user=User.find(params[:id])
+
+    if user.delete
+      render json: {status: "Se eliminó el usuario"}, status: :ok
+    else
+      render json: {status: "No se pudo eliminar el usuario"}, status: :unprocessable_entity
+    end
+
+  end
+
   #Crear un nuevo usuario
   def create
     user = User.new(user_params)
     if user.save
-        render json:{status: "Se creó el usuario"}, status: :ok
+        render json:{status: "Se creó el usuario."}, status: :ok
     else
-        render json: {status: "Error al guardar", errors: user.errors}, status: :unprocessable_entity
+        render json: {status: "Error al guardar.", errors: user.errors}, status: :unprocessable_entity
     end
+  end
+  #Actualizar un usuario
+  def update
+    user = User.find(params[:id])
+    #user.name = params[:name]
+    #user.phone = params[:phone]
+
+    if user.update(user_params)
+      render json: {status: "Se actualizó el usuario."}, status: :ok
+    else
+      render json: {status: "Error al actualizar."}, status: :unprocessable_entity
+    end
+
   end
 
   def user_params
